ac429f1e9b5d226504bcfee3ec54f4fcd947b62a
sagar-gsharp <sagar@gsharp.media>
2026-01-20 18:42:17 +0530

    fix

--- app.ts
+++ app.ts
@@ -26,6 +26,7 @@ const corsOptions: CorsOptions = {
         "http://localhost:5173",
         "http://localhost:5173/",
         "http://localhost:3002",
+        "http://localhost:3000",
         "http://localhost:5174",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:3001",
