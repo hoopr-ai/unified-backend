ed349b098d1d053fd8e8e44224f4f95fe50a91b3
sagar-gsharp <sagar@gsharp.media>
2026-01-21 13:20:37 +0530

    authentication file updated user App Error to throw Errors

--- middlewares/authenticate.ts
+++ middlewares/authenticate.ts
@@ -1,6 +1,7 @@
 import type { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import type { JwtPayload } from "jsonwebtoken";
+import { AppError } from "../services/helper-service/AppError";
 interface AuthRequest extends Request {
   session?: string | JwtPayload;
 }
@@ -9,34 +10,22 @@ export const authenticate = (req: AuthRequest, res: Response, next: NextFunction
   try {
     const authHeader = req.headers.authorization;
     const token = authHeader?.split(" ")[1];
-
     if (!token) {
-      return res.status(401).json({
-        data: {},
-        error: { code: 1, message: "Sign in required to access this resource." },
-      });
+      throw new AppError("The JWT token provided is invalid.", 401);
     }
-
     jwt.verify(
       token,
       process.env.JWT_SECRET_KEY as string,
       (err, decoded) => {
         if (err || !decoded) {
-          return res.status(401).json({
-            data: {},
-            error: { code: 1, message: "The JWT token provided is invalid." },
-          });
+          throw new AppError("The JWT token provided is invalid.", 401);
         }
-
         req.session = decoded;
         next();
       }
     );
   } catch (error) {
-    res.status(500).json({
-      data: {},
-      error: { code: 1, message: "Authentication error occurred." },
-    });
+    throw new AppError("The JWT token provided is invalid.", 401);
   }
 };
 
