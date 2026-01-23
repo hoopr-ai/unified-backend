9b7fe553382000c2df1f067428bcb3feb15fe946
sagar-gsharp <sagar@gsharp.media>
2026-01-23 11:26:17 +0530

    track by code middleware added

--- routes/track.route.ts
+++ routes/track.route.ts
@@ -3,10 +3,12 @@ import {
   getAllTracks,
   getTracksByCodes,
 } from "../controllers/track.controller";
+import { validateRequest } from "../middlewares/validateRequest";
+import { getTracksByCodesRequestSchema } from "../middlewares/track.validation";
 
 const router = Router();
 
 router.get("/", getAllTracks);
-router.post("/by-codes", getTracksByCodes);
+router.post("/by-codes", validateRequest(getTracksByCodesRequestSchema), getTracksByCodes);
 
 export default router;
