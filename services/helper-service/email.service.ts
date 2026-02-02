2bead2e3d460899b674fe692f6ceea19d07d0e73
sagar-gsharp <sagar@gsharp.media>
2026-02-02 14:49:08 +0530

    Revert "nodemailer"
    This reverts commit 07595548d91895661ccea5bd3a37efd1710e4a2b.

--- services/helper-service/email.service.ts
+++ services/helper-service/email.service.ts
@@ -5,8 +5,8 @@ const transporter = nodemailer.createTransport({
   port: 587,
   secure: false,
   auth: {
-    user: 'infra@gsharp.media',
-    pass: 'tbdp wxgb pgty amjm',
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
   },
 });
 
