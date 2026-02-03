6b6b55c12469e1d431db1175cc7d27f3d88ceebe
sagar-gsharp <sagar@gsharp.media>
2026-02-03 14:24:47 +0530

    env log

--- app.ts
+++ app.ts
@@ -25,7 +25,7 @@ app.use(cors(corsOptions));
 app.options(/.*/, cors(corsOptions));
 
 app.use(activityLoggerMiddleware());
-console.log("PROJECT_ID at startup:", process.env.PROJECT_ID);
+// console.log("PROJECT_ID at startup:", process.env.PROJECT_ID);
 
 app.use("/user", userRoutes);
 app.use("/filters", filterRoutes);
