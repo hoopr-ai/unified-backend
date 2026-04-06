56f5987560662008835fc34213461918385bac5b
sagar-gsharp <sagar@gsharp.media>
2026-04-06 12:31:51 +0530

    updated cors config

--- services/helper-service/cors.config.ts
+++ services/helper-service/cors.config.ts
@@ -9,6 +9,7 @@ export const getCorsOptions = (): CorsOptions => {
   const allowedOrigins: string[] = [
     frontendUrl,
     "http://localhost:5173",
+    "https://internal.hoopr.ai/",
     "http://localhost:3002",
     "http://localhost:3000",
     "http://localhost:5174",
