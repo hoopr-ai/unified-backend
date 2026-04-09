337be141e76d4ddecabe763cecd7d30678a72cfc
sagar-gsharp <sagar@gsharp.media>
2026-04-09 14:59:10 +0530

    RefreshTokenExpiry updated

--- services/dto-service/user/user-auth.dto.ts
+++ services/dto-service/user/user-auth.dto.ts
@@ -82,8 +82,8 @@ export interface LoginResponse {
 export const AccessTokenExpiry = "3h";
 export const AccessTokenExpiryInSeconds = 3 * 60 * 60;
 
-export const RefreshTokenExpiry = "7d";
-export const RefreshTokenExpiryInSeconds = 7 * 24 * 60 * 60;
+export const RefreshTokenExpiry = "30d";
+export const RefreshTokenExpiryInSeconds = 30 * 24 * 60 * 60;
 
 export interface SessionMetadata {
   ipAddress?: string;
